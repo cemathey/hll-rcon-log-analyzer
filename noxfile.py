54915d15a88c93f5c4fb8758f796c798e8b706c9
Christopher Mathey <emathey@protonmail.com>
2022-08-29 13:09:37 +0400

    Set up linting

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,70 @@
-from re import A
+import tempfile
+
 import nox
+
+locations_to_lint = "src", "tests", "noxfile.py"
+locations_to_black = locations_to_lint
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.10"])
 def tests(session):
     args = session.posargs or ["--cov"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session,
+        "coverage[toml]",
+        "pytest",
+        "pytest-cov",
+    )
     session.run("pytest", *args)
+
+
+@nox.session(python=["3.10"])
+def black(session):
+    args = session.posargs or locations_to_black
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+
+
+@nox.session(python=["3.10"])
+def lint(session):
+    args = session.posargs or locations_to_lint
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python=["3.10"])
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")

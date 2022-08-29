134e650114fba5037361e93aa64b159c837cdb6d
Christopher Mathey <emathey@protonmail.com>
2022-08-29 11:53:36 +0400

    Set up unit testing and code coverage.

---
+++ noxfile.py
@@ -0,0 +1,9 @@
+from re import A
+import nox
+
+
+@nox.session(python=["3.10"])
+def tests(session):
+    args = session.posargs or ["--cov"]
+    session.run("poetry", "install", external=True)
+    session.run("pytest", *args)

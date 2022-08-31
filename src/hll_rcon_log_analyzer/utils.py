33530532ec26089d89297e601794677a36d3c543
Christopher Mathey <emathey@protonmail.com>
2022-08-31 14:40:59 +0400

    Convert timestamps from strings to Timestamp on import.

--- src/hll_rcon_log_analyzer/utils.py
+++ src/hll_rcon_log_analyzer/utils.py
@@ -5,6 +5,6 @@ import pandas as pd
 def load_logs(file_path: str) -> pd.DataFrame:
     """Return a dataframe from the given path to a historical log file from community RCON."""
     with open(file_path) as csvfile:
-        df = pd.read_csv(csvfile)
+        df = pd.read_csv(csvfile, parse_dates=["event_time"])
 
     return df

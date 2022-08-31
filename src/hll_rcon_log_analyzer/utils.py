7569c79e9eeeb0698e24b6af021479f7b76151f8
Christopher Mathey <emathey@protonmail.com>
2022-08-31 12:37:08 +0400

    Add a helper method to load CRCON csv files into a Pandas DF.

---
+++ src/hll_rcon_log_analyzer/utils.py
@@ -0,0 +1,10 @@
+"""Common utility functions such as parsing RCON log csv files."""
+import pandas as pd
+
+
+def load_logs(file_path: str) -> pd.DataFrame:
+    """Return a dataframe from the given path to a historical log file from community RCON."""
+    with open(file_path) as csvfile:
+        df = pd.read_csv(csvfile)
+
+    return df

9f41b65a56867f3c72215bbe8ec8dbede63765f0
Christopher Mathey <emathey@protonmail.com>
2022-09-05 11:53:21 +0400

    Add config.py for config file parsing and basic validation

---
+++ src/hll_rcon_log_analyzer/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import tomli
+
+from hll_rcon_log_analyzer.constants import (
+    CONFIG_FILE_NAME,
+    PROJECT_ROOT_PATH,
+    COMMUNITY_RCON_LOG_ACTIONS,
+)
+
+full_path = Path(PROJECT_ROOT_PATH) / "config" / CONFIG_FILE_NAME
+
+with open(full_path, mode="rb") as fp:
+    config = tomli.load(fp)
+
+    for event in (
+        config["community_rcon"]["selected_events"]
+        + config["community_rcon"]["clearing_events"]
+    ):
+        if event not in COMMUNITY_RCON_LOG_ACTIONS:
+            raise ValueError(f"{event} in {full_path} is not a valid log action.")
